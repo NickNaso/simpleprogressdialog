f08a04b8c43b79ba848f9df60de6339826a2f458
Nacios Technologies <info@nacios.it>
2013-10-26 19:48:33 +0200

    Update simpleProgressDialog.js
    Aggiunto slash (/) per commenti

--- js/simpleProgressDialog.js
+++ js/simpleProgressDialog.js
@@ -1,4 +1,4 @@
-*******************************************************************************
+/*******************************************************************************
 * Copyright (c) 2012, 2013 Nacios Technologies.
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Apache License Version 2.0
